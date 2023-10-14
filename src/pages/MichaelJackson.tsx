18485d666e842f9041799aea38221f14160a5f42
jeeveshmahato <jeeveshmahato100@gmail.com>
2023-10-14 13:15:14 +0530

    first commit

---
+++ src/pages/MichaelJackson.tsx
@@ -0,0 +1,330 @@
+import { FunctionComponent } from "react";
+import styles from "./MichaelJackson.module.css";
+
+const MichaelJackson: FunctionComponent = () => {
+  return (
+    <div className={styles.michaelJackson}>
+      <div className={styles.financialAccountingFinancialContainer}>
+        <ul className={styles.financialAccountingFinancial}>
+          <li className={styles.financialAccounting}>Financial accounting</li>
+          <li className={styles.financialAccounting}>Financial statements</li>
+          <li className={styles.financialAccounting}>Bookkeeping</li>
+          <li className={styles.financialAccounting}>Accounting and finance</li>
+          <li className={styles.financialAccounting}>Corporate Finance</li>
+          <li className={styles.financialAccounting}>
+            Maintain Charts of Accounts
+          </li>
+          <li className={styles.financialAccounting}>
+            Profit and loss statements
+          </li>
+          <li className={styles.financialAccounting}>Bank Reconciliation</li>
+          <li>Balance Sheets</li>
+        </ul>
+      </div>
+      <div className={styles.fromParent}>
+        <b className={styles.from}>FROM</b>
+        <div className={styles.india}>INDIA</div>
+      </div>
+      <b className={styles.servicesIOffer}>SERVICES I OFFER</b>
+      <div className={styles.aboutParent}>
+        <b className={styles.about}>ABOUT</b>
+        <div className={styles.iAmA}>
+          I am a Professional Charted Accountant here to offer professional
+          services of accounting and finance, financial statements, Bookkeeping
+          in affordable price.
+        </div>
+      </div>
+      <div className={styles.partnerSinceParent}>
+        <b className={styles.partnerSince}>PARTNER SINCE</b>
+        <div className={styles.div}>2011</div>
+      </div>
+      <div className={styles.averageResponseTimeParent}>
+        <b className={styles.averageResponseTime}>AVERAGE RESPONSE TIME</b>
+        <div className={styles.minutes}>30 minutes</div>
+      </div>
+      <div className={styles.whyMeParent}>
+        <b className={styles.averageResponseTime}>WHY ME?</b>
+        <div className={styles.oneTimeDelivery247Container}>
+          <ul className={styles.financialAccountingFinancial}>
+            <li className={styles.financialAccounting}>One-time delivery</li>
+            <li className={styles.financialAccounting}>
+              24/7 customer support
+            </li>
+            <li>Error-free documents</li>
+          </ul>
+        </div>
+      </div>
+      <b className={styles.aboutMichaelJackson}>About Michael Jackson</b>
+      <div className={styles.topBar}>
+        <div className={styles.overallOuterBox} />
+        <div className={styles.signUpButton}>
+          <button className={styles.signUp}>Login</button>
+        </div>
+        <div className={styles.signUpButton1}>
+          <button className={styles.signUp1}>Register</button>
+        </div>
+        <button className={styles.services}>
+          <b className={styles.text}>Blogs</b>
+        </button>
+        <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+        <div className={styles.aboutGroup}>
+          <button className={styles.solutions}>About</button>
+          <img className={styles.downArrowIcon} alt="" src="/down-arrow.svg" />
+        </div>
+        <div className={styles.solutionsParent}>
+          <button className={styles.solutions}>Solutions</button>
+          <img className={styles.downArrowIcon} alt="" src="/down-arrow.svg" />
+        </div>
+        <div className={styles.featuresParent}>
+          <button className={styles.solutions}>Features</button>
+          <img className={styles.downArrowIcon} alt="" src="/down-arrow.svg" />
+        </div>
+      </div>
+      <img
+        className={styles.michaelJacksonChild}
+        alt=""
+        src="/group-35375.svg"
+      />
+      <div className={styles.recommendedForYouParent}>
+        <b className={styles.recommendedForYou}>Recommended for you</b>
+        <div className={styles.rectangleParent}>
+          <div className={styles.groupChild} />
+          <img
+            className={styles.groupItem}
+            alt=""
+            src="/rectangle-376@2x.png"
+          />
+          <b className={styles.michaelJackson1}>Michael Jackson</b>
+          <div className={styles.iWillDo}>
+            I will do business evaluation and corporate services
+          </div>
+          <b className={styles.b}>₹4,370</b>
+          <div className={styles.parent}>
+            <div className={styles.div1}>
+              <b>{`4.8 `}</b>
+              <span className={styles.span}>(89)</span>
+            </div>
+            <img className={styles.starFillIcon} alt="" src="/starfill.svg" />
+          </div>
+          <div className={styles.component1}>
+            <button className={styles.signUp1}>View services</button>
+          </div>
+          <div className={styles.groupInner} />
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="/rectangle-376@2x.png"
+          />
+          <b className={styles.stevieWonder}>Stevie Wonder</b>
+          <div className={styles.iWillDo1}>
+            I will do business evaluation and corporate services
+          </div>
+          <b className={styles.b1}>₹4,263</b>
+          <div className={styles.group}>
+            <div className={styles.div1}>
+              <b>{`5.0 `}</b>
+              <span className={styles.span}>(62)</span>
+            </div>
+            <img className={styles.starFillIcon} alt="" src="/starfill.svg" />
+          </div>
+          <div className={styles.component11}>
+            <button className={styles.signUp1}>View services</button>
+          </div>
+          <div className={styles.rectangleDiv} />
+          <img
+            className={styles.groupChild1}
+            alt=""
+            src="/rectangle-376@2x.png"
+          />
+          <b className={styles.rayCharles}>Ray Charles</b>
+          <div className={styles.iWillDo2}>
+            I will do business evaluation and corporate services
+          </div>
+          <b className={styles.b2}>₹2,586</b>
+          <div className={styles.container}>
+            <div className={styles.div1}>
+              <b>{`4.3 `}</b>
+              <span className={styles.span}>(189)</span>
+            </div>
+            <img className={styles.starFillIcon} alt="" src="/starfill.svg" />
+          </div>
+          <div className={styles.component12}>
+            <button className={styles.signUp1}>View services</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footer1}>
+          <a
+            className={styles.f}
+            href="https://www.facebook.com/search/top?q=mohitetax"
+            target="_blank"
+          >
+            f
+          </a>
+          <a
+            className={styles.f1}
+            href="https://www.instagram.com/mohitetax_/"
+            target="_blank"
+          >
+            f
+          </a>
+          <div className={styles.indiasFirstPlatform}>
+            India's first platform dedicated to simplifying partner search
+          </div>
+          <div className={styles.sambhajiNagarNo2}>
+            Sambhaji Nagar, No2, St. Antony Road, Chembur, Mumbai - 400071,
+            Maharashtra
+          </div>
+          <div className={styles.footerssocialLinkParent}>
+            <div className={styles.footerssocialLink}>
+              <img
+                className={styles.downArrowIcon}
+                alt=""
+                src="/footerssocial-icon.svg"
+              />
+            </div>
+            <div className={styles.footerssocialLink1}>
+              <img
+                className={styles.downArrowIcon}
+                alt=""
+                src="/footerssocial-icon1.svg"
+              />
+            </div>
+            <div className={styles.footerssocialLink2}>
+              <img
+                className={styles.downArrowIcon}
+                alt=""
+                src="/footerssocial-icon2.svg"
+              />
+            </div>
+            <div className={styles.linkedinLogo}>
+              <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+              <a
+                className={styles.f2}
+                href="https://www.instagram.com/mohitetax_/"
+                target="_blank"
+              >
+                f
+              </a>
+            </div>
+          </div>
+          <div className={styles.resourcesParent}>
+            <b className={styles.resources}>RESOURCES</b>
+            <div className={styles.text4}>
+              <button className={styles.pricing}>Blogs</button>
+            </div>
+            <div className={styles.text6}>
+              <button className={styles.pricing}>Forms</button>
+            </div>
+          </div>
+          <div className={styles.supportParent}>
+            <b className={styles.support}>SUPPORT</b>
+            <div className={styles.text4}>
+              <button className={styles.pricing}>Help</button>
+            </div>
+            <div className={styles.text6}>
+              <button className={styles.pricing}>Contact Us</button>
+            </div>
+          </div>
+          <div className={styles.legalParent}>
+            <b className={styles.legal}>LEGAL</b>
+            <div className={styles.text4}>
+              <button className={styles.pricing}>Privacy</button>
+            </div>
+            <div className={styles.text6}>
+              <button className={styles.pricing}>Terms</button>
+            </div>
+            <div className={styles.text16}>
+              <button className={styles.pricing}>Accessibility</button>
+            </div>
+          </div>
+          <div className={styles.solutionsGroup}>
+            <b className={styles.company}>SOLUTIONS</b>
+            <div className={styles.text4}>
+              <button className={styles.pricing}>Search</button>
+            </div>
+            <div className={styles.text6}>
+              <button className={styles.pricing}>Connect</button>
+            </div>
+            <div className={styles.text16}>
+              <button className={styles.text23}>Research</button>
+            </div>
+            <div className={styles.text24}>
+              <button className={styles.text23}>Academy</button>
+            </div>
+          </div>
+          <div className={styles.companyParent}>
+            <b className={styles.company}>COMPANY</b>
+            <div className={styles.text4}>
+              <button className={styles.pricing}>{`About `}</button>
+            </div>
+            <div className={styles.text6}>
+              <button className={styles.pricing}>Pricing</button>
+            </div>
+            <div className={styles.text16}>
+              <button className={styles.pricing}>Careers</button>
+            </div>
+          </div>
+          <img className={styles.footerChild} alt="" src="/vector-195.svg" />
+          <img className={styles.image1Icon1} alt="" src="/image-11@2x.png" />
+        </div>
+        <div className={styles.copyrightFooter}>
+          <div className={styles.registeredTrademarkOf}>
+            Registered trademark of India Private Limited © 2023 loerumipsum
+            Inc. All Rights Reserved
+          </div>
+        </div>
+      </div>
+      <div className={styles.michaelJacksonParent}>
+        <b className={styles.michaelJackson2}>Michael Jackson</b>
+        <div className={styles.iAmHereContainer}>
+          <p
+            className={styles.iAmHere}
+          >{`I am here to provide my expertise in accounting and finance, which includes financial statements, economics, and auditing, all to assist `}</p>
+          <p className={styles.iAmHere}>you effectively</p>
+        </div>
+        <div className={styles.groupDiv}>
+          <div className={styles.div4}>
+            <b>{`4.8 `}</b>
+            <span className={styles.span}>(89)</span>
+          </div>
+          <img className={styles.starFillIcon3} alt="" src="/starfill.svg" />
+        </div>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.groupChild2} />
+          <b className={styles.whatPeopleSay}>What people say?</b>
+          <div className={styles.iCannotExpress}>
+            I cannot express enough gratitude for the support Micheal has
+            provided in managing my personal finances. Their attention to detail
+            and deep understanding of financial markets has ensured that my
+            investments are in safe hands. I highly recommend their services to
+            anyone seeking financial guidance.
+          </div>
+          <img className={styles.groupIcon} alt="" src="/group-35370.svg" />
+        </div>
+        <div className={styles.frameChild} />
+        <div className={styles.basicToComplex}>Basic to complex tasks</div>
+        <b className={styles.b3}>₹4,370</b>
+        <div className={styles.component13}>
+          <button className={styles.signUp1}>Request Proposal</button>
+        </div>
+        <div className={styles.deliversTheJobWithin2DaysParent}>
+          <div className={styles.deliversTheJob}>
+            Delivers the job within 2 days
+          </div>
+          <img
+            className={styles.calendar2LineIcon}
+            alt=""
+            src="/calendar2line.svg"
+          />
+        </div>
+        <div className={styles.component7}>
+          <button className={styles.signUp}>Chat with me</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MichaelJackson;
